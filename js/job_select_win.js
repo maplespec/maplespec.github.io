8d4bfa53ffee8d2dcd54ecc714963de9338fdb65
maplespec <tkdfuf0923@naver.com>
2019-12-08 23:52:17 +0900

    Commit

--- js/job_select_win.js
+++ js/job_select_win.js
@@ -53,11 +53,6 @@ $(document).ready(function() {
 
     $(".job_img").click(function() {
         selected_job = $(this).attr("id");
-        /*if(selected_job == "xenon") {
-            console.log(selected_job + "은 선택 불가능");
-        } else {
-            console.log(selected_job + "을 선택함");
-        }*/
         $("#job_select_popup_title").css("background-image", job_title_img_path + selected_job + path_end);
         if(selected_job == "xenon") {
             $("#job_popup_area").css("display", "block");
@@ -79,14 +74,15 @@ $(document).ready(function() {
         $("#xenon_select_popup").css("display", "none");
     });
 
-    /*var item_img_exception = [1, 2, 13, 14, 16, 17, 19];
-        var item_img = $(".item_img").eq(i);
-        if (item_img_exception.indexOf(i) != -1) {
-            continue;
-        }*/
-    // 엠블, 모자+상의+하의, 무기, 보조, 포켓
     setInterval(function() {
-        // 엠블렘
+        var cap_img = $(".item_img").eq(1);
+        var emblem_img = $(".item_img").eq(2);
+        var weapon_img = $(".item_img").eq(13);
+        var clothes_img = $(".item_img").eq(14);
+        var sub_weapon_img = $(".item_img").eq(16);
+        var poket_img = $(".item_img").eq(17);
+        var pants_img = $(".item_img").eq(19);
+        
         var adventurer_warrior = ["hero", "paladin", "darknight"];
         var adventurer_magician = ["bishop", "arch_mage_fire", "arch_mage_ice"];
         var adventurer_bowman = ["bow_master", "marksman", "pathfinder"];
@@ -95,13 +91,56 @@ $(document).ready(function() {
         var cygnus = ["mikhail", "soul_master", "flame_wizard", "wind_breaker", "night_walker", "striker"];
         var hero = ["aran", "luminous", "evan", "mercedes", "phantom", "eunwol"];
         var resistance = ["blaster", "battlemage", "wild_hunter", "mechanic"];
-        var demon = ["demon_slayer", "demon_avenger"];
+        var demon = ["demon"]; // "demon_avenger"은 아직 미지원, 지원 전까지 "demon_slyaer" = "demon"
         var nova = ["kaiser", "cadena", "angelic_burster"];
         var lef = ["illium", "ark"];
-        //"zero", "kinesis", "ho_young"은 따로 / "xenon"은 미지원
+        //"zero", "kinesis", "ho_young"은 따로 / "xenon"은 아직 미지원
+
+        var emblem_adventurer = adventurer_warrior.concat(adventurer_magician, adventurer_bowman, adventurer_thief, adventurer_pirate);
+        var armor_warrior = adventurer_warrior.concat(cygnus[0], cygnus[1], hero[0], resistance[0], demon[0], nova[0], "zero");
+        var armor_magician = adventurer_magician.concat(cygnus[2], hero[1], hero[2], resistance[1], lef[0], "kinesis");
+        var armor_bowman = adventurer_bowman.concat(cygnus[3], hero[3], resistance[2]);
+        var armor_thief = adventurer_thief.concat(cygnus[4], hero[4], nova[1], "ho_young");
+        var armor_pirate = adventurer_pirate.concat(cygnus[5], hero[5], resistance[3], nova[2], lef[1]);
         
-        if(selected_job == "aran") {
-            $(".item_img").eq(17).css("background-image", item_img_path + "poket_str" + path_end);
+        var poket_str = adventurer_warrior.concat(adventurer_pirate[1], adventurer_pirate[2], cygnus[0], cygnus[1], cygnus[5], hero[0],
+                                                  hero[5], resistance[0], demon[0], nova[0], lef[1], "zero");
+        var poket_dex = adventurer_bowman.concat(adventurer_pirate[0], cygnus[3], hero[3], resistance[2], resistance[3], nova[2]);
+        var poket_int = adventurer_magician.concat(cygnus[2], hero[1], hero[2], resistance[1], lef[0], "kinesis");
+        var poket_luk = adventurer_thief.concat(cygnus[4], hero[4], nova[1], "ho_young");
+
+
+
+        if(armor_warrior.indexOf(selected_job) != -1) {
+            cap_img.css("background-image", item_img_path + "warrior_cap" + path_end);
+            clothes_img.css("background-image", item_img_path + "warrior_clothes" + path_end);
+            pants_img.css("background-image", item_img_path + "warrior_pants" + path_end);
+        } else if(armor_magician.indexOf(selected_job) != -1) {
+            cap_img.css("background-image", item_img_path + "magician_cap" + path_end);
+            clothes_img.css("background-image", item_img_path + "magician_clothes" + path_end);
+            pants_img.css("background-image", item_img_path + "magician_pants" + path_end);
+        } else if(armor_bowman.indexOf(selected_job) != -1) {
+            cap_img.css("background-image", item_img_path + "bowman_cap" + path_end);
+            clothes_img.css("background-image", item_img_path + "bowman_clothes" + path_end);
+            pants_img.css("background-image", item_img_path + "bowman_pants" + path_end);
+        } else if(armor_thief.indexOf(selected_job) != -1) {
+            cap_img.css("background-image", item_img_path + "thief_cap" + path_end);
+            clothes_img.css("background-image", item_img_path + "thief_clothes" + path_end);
+            pants_img.css("bacskground-image", item_img_path + "thief_pants" + path_end);
+        } else if(armor_pirate.indexOf(selected_job) != -1) {
+            cap_img.css("background-image", item_img_path + "pirate_cap" + path_end);
+            clothes_img.css("background-image", item_img_path + "pirate_clothes" + path_end);
+            pants_img.css("background-image", item_img_path + "pirate_pants" + path_end);
+        }
+
+        if(poket_str.indexOf(selected_job) != -1) {
+            poket_img.css("background-image", item_img_path + "poket_str" + path_end);
+        } else if(poket_dex.indexOf(selected_job) != -1) {
+            poket_img.css("background-image", item_img_path + "poket_dex" + path_end);
+        } else if(poket_int.indexOf(selected_job) != -1) {
+            poket_img.css("background-image", item_img_path + "poket_int" + path_end);
+        } else if(poket_luk.indexOf(selected_job) != -1) {
+            poket_img.css("background-image", item_img_path + "poket_luk" + path_end);
         }
     }, 10);
 
